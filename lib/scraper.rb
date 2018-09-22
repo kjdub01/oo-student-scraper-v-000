45f4be3133610057276f1765c9105f62b827e24c
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-22 22:36:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     site = Nokogiri::HTML(open(index_url))
     student_list = []
-      site.css
+      site.css()
     name:
     location:
     profile_url:
